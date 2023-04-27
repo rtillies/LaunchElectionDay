7d3ab5baf5bb1537977c1d13136e4604c719927f
Richard T <richard@turing.edu>
2023-04-27 09:56:11 -0400

    Runner file for demo and presentation

--- LaunchElectionDay/Program.cs
+++ LaunchElectionDay/Program.cs
@@ -1,34 +1,15 @@
 ﻿using System.Diagnostics;
 using LaunchElectionDay;
 
-// New Candidate
-var diana = new Candidate("Diana D", "Democrat");
+var thisElection = CreateElection();
+Console.WriteLine($"Welcome to Election {thisElection.GetYear()}!");
+int option = -1;
 
-// Display Candidate Name, Party, Vote Count
-Console.WriteLine(diana.Name);
-Console.WriteLine(diana.Party);
-Console.WriteLine(diana.Votes);
-
-// Candidate Votes 3x
-diana.VoteFor();
-diana.VoteFor();
-diana.VoteFor();
-
-// Display Candidate Vote Count
-Console.WriteLine(diana.Votes);
-
-// New Race
-var race = new Race("City Council District 10");
-
-// Display Race Office and current Candidates
-Console.WriteLine(race.Office);
-displayCandidates(race);
-
-// Register Candidate
-race.RegisterCandidate(diana);
-
-// Display current Candidates
-displayCandidates(race);
+while (option != 0)
+{
+    option = MainMenu(thisElection);
+    processSelection(thisElection, option);
+}
 
 static void displayCandidates(Race race)
 {
@@ -36,5 +17,113 @@ static void displayCandidates(Race race)
     foreach (var c in race.Candidates)
     {
         Console.WriteLine($"{c.Name}, {c.Party}, {c.Votes} votes");
+    }
+}
+
+static void displayRaces(Election elect)
+{
+    var races = elect.GetRaces();
+    Console.WriteLine($"Race count: {races.Count}");
+    foreach (var race in races)
+    {
+        var raceOpen = race.IsOpen ? "Open" : "Closed";
+        Console.WriteLine($"{race.Office}: {raceOpen}, Candidates: {race.Candidates.Count}");
+    }
+}
+
+static Election CreateElection()
+{
+    var election = new Election("2023");
+    var cityCouncil = new Race("City Council District 10");
+    var mayor = new Race("Raleigh Mayoral Race");
+
+    var diana = new Candidate("Diana D", "Democrat");
+    var camilla = new Candidate("Camilla B", "Libertarian");
+
+    var barack = new Candidate("Barack O", "Democrat");
+    var bernie = new Candidate("Bernie S", "Independent");
+    var george = new Candidate("George W", "Republican");
+
+    cityCouncil.RegisterCandidate(diana);
+    cityCouncil.RegisterCandidate(camilla);
+
+    mayor.RegisterCandidate(barack);
+    mayor.RegisterCandidate(bernie);
+    mayor.RegisterCandidate(george);
+
+    election.AddRace(cityCouncil);
+    election.AddRace(mayor);
+
+    CastRandomVotes(election);
+
+    return election;
+}
+
+static void CastRandomVotes(Election elect)
+{
+    foreach(var candidate in elect.GetAllCandidates())
+    {
+        Random rnd = new Random();
+        int num = rnd.Next(50);
+        for (int i = 0; i < num; i++)
+        {
+            candidate.VoteFor();
+        }
+    }
+}
+
+static int MainMenu(Election elect)
+{
+    Console.WriteLine();
+    Console.WriteLine("Main Menu");
+    Console.WriteLine("0 - Quit");
+    Console.WriteLine("1 - List All Races");
+    Console.WriteLine("2 - List All Candidates");
+    Console.WriteLine("3 - End Election");
+    Console.WriteLine("4 - Vote For Candidate");
+
+    Console.Write("Select an option 0-4: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+static void processSelection(Election elect, int option)
+{
+    Console.WriteLine();
+
+    if (option == 0) // Quit
+    {
+        Console.WriteLine("Thank you for participating! Your vote matters!");
+    }
+    else if (option == 1) // All Races
+    {
+        displayRaces(elect);
+    }
+    else if (option == 2) // All Candidates
+    {
+        var races = elect.GetRaces();
+        foreach (var race in races)
+        {
+            var raceOpen = race.IsOpen ? "Open" : "Closed";
+            Console.WriteLine($"{race.Office}: {raceOpen}");
+            displayCandidates(race);
+            Console.WriteLine();
+        }
+    }
+    else if (option == 3) // End Election
+    {
+        var races = elect.GetRaces();
+        foreach (var race in races)
+        {
+            race.Close();
+        }
+        Console.WriteLine("All Races Closed!");
+    }
+    else if (option == 4) // Vote for candidate
+    {
+        Console.WriteLine("This function is under construction.");
+    }
+    else // Invalid entry
+    {
+        Console.WriteLine("Invalid entry, please try again.");
     }
 }
